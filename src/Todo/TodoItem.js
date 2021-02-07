73fbd93224ee5d57237312faa9b41beff4ef8cd1
Yauheni <Karasevgen1205@yandex.ru>
2021-02-07 21:29:37 +0300

    My first JavaScript project

---
+++ src/Todo/TodoItem.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const styles = {
+    li: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '.5rem 1 rem',
+        border: '1px solid #ccc',
+        borderRadius: '1px',
+        marginBottom: '.5rem'
+    },
+    input: {
+        marginRight: '1rm'
+    }
+}
+
+function TodoItem({ todo, index, onChange}) {
+    return (
+        <li style={styles.li}>
+            <span>
+                <input 
+                    type='checkbox' 
+                    style={styles.input} 
+                    onChange={() => onChange(todo.id)}
+                />
+                <strong>{index + 1}</strong>
+                &nbsp;
+                {todo.title}
+            </span>
+
+            <button className='rm'>&times;</button>
+        </li>
+    )
+} 
+
+TodoItem.propTypes = {
+    todo: PropTypes.object.isRequired,
+    index: PropTypes.number,
+    onChange: PropTypes.func.isRequired
+}
+
+export default TodoItem
