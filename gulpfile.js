d2897d8c2ccb61b4728d2ebdf33943d3085b6eb0
Yauheni <Karasevgen1205@yandex.ru>
2021-02-13 21:59:35 +0300

    add const in gulp and path 2

--- gulpfile.js
+++ gulpfile.js
@@ -29,21 +29,41 @@ const path = {
         js: distPath + "assets/js/",
         css: distPath + "assets/css/",
         images: distPath + "assets/images/",
-        fonts: distPath + "assets/fonts/",
+        fonts: distPath + "assets/fonts/"
     },
     src: {
         html: srcPath + "*.html",
         js: srcPath + "assets/js/*.js",
         css: srcPath + "assets/scss/*.scss",
         images: srcPath + "assets/images/**/*.{jpg,png,svg,gif,ico,webp,webmanifest,xml,json}",
-        fonts: srcPath + "assets/fonts/**/*.{eot,woff,woff2,ttf,svg}",
+        fonts: srcPath + "assets/fonts/**/*.{eot,woff,woff2,ttf,svg}"
     },
     watch: {
         html: srcPath + "**/*.html",
         js: srcPath + "assets/js/**/*.js",
         css: srcPath + "assets/scss/**/*.scss",
         images: srcPath + "assets/images/**/*.{jpg,png,svg,gif,ico,webp,webmanifest,xml,json}",
-        fonts: srcPath + "assets/fonts/**/*.{eot,woff,woff2,ttf,svg}",
+        fonts: srcPath + "assets/fonts/**/*.{eot,woff,woff2,ttf,svg}"
     },
     clean: "./" + distPath
+}
+
+/* Tasks */
+
+function serve() {
+    browserSyns.init({
+        server: {
+            baseDir: "./" + distPath
+        }
+    })
+}
+
+function html(cd) {
+    panini.refresh();
+    return src(path.src.html, {base: srcPath})
+        .pipe(plumber())
+        .pipe(panini({
+            root: srcPath,
+
+        }))
 }
