a0b99401749a4729658276d7c206cc3006251bc7
Yauheni <Karasevgen1205@yandex.ru>
2021-02-13 21:45:41 +0300

    add const in gulp and path 1

--- gulpfile.js
+++ gulpfile.js
@@ -27,5 +27,23 @@ const path = {
     build: {
         html: distPath,
         js: distPath + "assets/js/",
-    }
+        css: distPath + "assets/css/",
+        images: distPath + "assets/images/",
+        fonts: distPath + "assets/fonts/",
+    },
+    src: {
+        html: srcPath + "*.html",
+        js: srcPath + "assets/js/*.js",
+        css: srcPath + "assets/scss/*.scss",
+        images: srcPath + "assets/images/**/*.{jpg,png,svg,gif,ico,webp,webmanifest,xml,json}",
+        fonts: srcPath + "assets/fonts/**/*.{eot,woff,woff2,ttf,svg}",
+    },
+    watch: {
+        html: srcPath + "**/*.html",
+        js: srcPath + "assets/js/**/*.js",
+        css: srcPath + "assets/scss/**/*.scss",
+        images: srcPath + "assets/images/**/*.{jpg,png,svg,gif,ico,webp,webmanifest,xml,json}",
+        fonts: srcPath + "assets/fonts/**/*.{eot,woff,woff2,ttf,svg}",
+    },
+    clean: "./" + distPath
 }
